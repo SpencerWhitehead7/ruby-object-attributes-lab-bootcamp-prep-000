f17d2a7a8223301c1527b0177c2939b94d4c8bdb
Spencer Whitehead <spencerwhitehead7@gmail.com>
2018-02-10 22:31:50 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,13 +5,14 @@ class Dog
   def name
     @name
   end
+  def name=(new_name)
+    @name = new_name
+  end
   def breed=(dog_breed)
     @breed = dog_breed
   end
   def breed
     @breed
   end
-  def name=(new_name)
-    @name = new_name
-  end
+
 end
